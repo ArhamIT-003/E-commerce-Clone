a17424b4a07dcb589645bfdbe2a9e31d905b17d3
Arham <muhammadarham59@gmail.com>
2023-07-27 07:10:43 -0700

    another commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="right"></div>
+      <div className="center"></div>
+
+      <div className="left"></div>
+    </div>
+  );
 };
 
 export default Navbar;
